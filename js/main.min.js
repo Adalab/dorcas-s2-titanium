3bfcada1b0e0b72784c4b315845c2f49b74c3642
АНЯ <annya.branco@gmail.com>
2018-07-05 11:53:44 +0200

    Update: icons with link

--- js/main.min.js
+++ js/main.min.js
@@ -2,21 +2,14 @@
 
 var arrow = document.querySelector('.fieldset__rellena--arrow');
 
+var mobilelLink = document.querySelector('.mobilelLink');
+var emailLink = document.querySelector('.emailLink');
+var linkedinLink = document.querySelector('.linkedinLink');
+var githubLink = document.querySelector('.githubLink');
+
+
 arrow.addEventListener('click', teste);
 
-/* ------ ANNA ------ */
-
-// // Variables para los iconos
-// var email = document.querySelector('.email');
-// var mobile = document.querySelector('.mobile');
-// var github = document.querySelector('.github');
-// var linkedin = document.querySelector('.linkedin');
-//
-// // Variables para los campos de form referentes a los contactos
-// var inputEmail = document.querySelector('.fieldset__rellena--input-email');
-// var inputMobile = document.querySelector('.fieldset__rellena--input-mobile');
-// var inputLinkedin = document.querySelector('.fieldset__rellena--input-linkedin');
-// var inputGithub = document.querySelector('.fieldset__rellena--input-github');
 
 var contact = document.querySelector('.contact');
 
@@ -25,37 +18,45 @@ contact.addEventListener('keyup', showIcon);
 
 
 function showIcon(event) {
-    if (event.target !== event.currentTarget) {
+  if (event.target !== event.currentTarget) {
 
+    notif(event.target.value);
+
+    var guilty = event.target.className;
+    var changeIcon;
+
+    if ( guilty.indexOf('email') >= 0 ){
+      var email2Link = event.target.value;
+      emailLink.href = 'mailto:' + email2Link;
+      changeIcon = 'email';
+
+    } else if ( guilty.indexOf('mobile') >= 0 ){
+      changeIcon = 'mobile';
+
+    } else if ( guilty.indexOf('linkedin') >= 0 ){
+      var linkedin2Link = event.target.value;
+      linkedinLink.href = 'http://linkedin.com/in/' + linkedin2Link;
+      changeIcon = 'linkedin';
+
+    } else if ( guilty.indexOf('github') >= 0 ){
+      var github2Link = event.target.value;
+      githubLink.href = 'https://github.com/' + github2Link;
+
+
+      changeIcon = 'github';
+    }
+
+    var icon2Toggle = document.querySelector("." + changeIcon);
+
+    if (event.target.value === "") {
+      icon2Toggle.style.display = 'none';
       notif(event.target.value);
-
-var guilty = event.target.className;
-var changeIcon;
-
-if ( guilty.indexOf('email') >= 0 ){
-  changeIcon = 'email';
-
-} else if ( guilty.indexOf('mobile') >= 0 ){
-  changeIcon = 'mobile';
-
-} else if ( guilty.indexOf('linkedin') >= 0 ){
-  changeIcon = 'linkedin';
-
-} else if ( guilty.indexOf('github') >= 0 ){
-  changeIcon = 'github';
-}
-
-var icon2Toggle = document.querySelector("." + changeIcon);
-
-   if (event.target.value === "") {
-     icon2Toggle.style.display = 'none';
-     notif(event.target.value);
-} else {
-  icon2Toggle.style.display = 'inline';
-     notif(event.target.value);
+    } else {
+      icon2Toggle.style.display = 'inline';
+      notif(event.target.value);
     }
     event.stopPropagation();
-}
+  }
 }
 
 //inputEmail.addEventListener('keyup', defEmail);

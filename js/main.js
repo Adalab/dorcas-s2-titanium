9a68c8212ab1eb01c7e469946dc0b9f646074725
Claudia <cl.calerolopez@gmail.com>
2018-07-06 11:08:34 +0200

    intento javascript claudia

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-var arrow = document.querySelector('.fieldset__rellena--arrow');
+'use strict';
 
-arrow.addEventListener('click', teste);
+var nameField = document.querySelector('.fieldset--name');
+var roleField = document.querySelector('.fieldset--role');
 
-function teste() {
-  alert('Activa alguna función en JavaScript');
+function writeData(event){
+  var guiltyElement = event.currenTarget;
+  var targetID = guiltyElement.getAttribute('data-donde');
+  document.querySelector('#' + targetID).innerHTML = guiltyElement.value;
 }
+nameField.addEventListener('keyup', writeData);
+roleField.addEventListener('keyup', writeData);

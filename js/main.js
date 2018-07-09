2f491d207c6c0b547b9da2ff66b5c3379458728c
Claudia <cl.calerolopez@gmail.com>
2018-07-09 12:52:49 +0200

    js nombre card

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ var nameField = document.querySelector('.fieldset--name');
 var roleField = document.querySelector('.fieldset--role');
 
 function writeData(event){
-  var guiltyElement = event.currenTarget;
+  var guiltyElement = event.currentTarget;
   var targetID = guiltyElement.getAttribute('data-donde');
-  document.querySelector('#' + targetID).innerHTML = guiltyElement.value;
+ document.querySelector('#' + targetID).innerHTML = guiltyElement.value;
 }
 nameField.addEventListener('keyup', writeData);
 roleField.addEventListener('keyup', writeData);

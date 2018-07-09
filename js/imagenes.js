70849dd5b973847983c58d21e4ade8c208baca3a
Albametnet <alhernandezsan@gmail.com>
2018-07-09 11:52:03 +0200

    act: js imagenes

--- js/imagenes.js
+++ js/imagenes.js
@@ -21,3 +21,25 @@ function fakeFileClick() {
 fileField.addEventListener('change', getImage);
 uploadBtn.addEventListener('click', fakeFileClick);
 
+
+
+// var fr = new FileReader();
+
+// var uploadBtn = document.querySelector('.añadir__upload-btn');
+// var fileField = document.querySelector('#img-selector');
+// var profileImage = document.querySelector('.mini');
+
+// function getImage(e){
+//   var myFile = e.currentTarget.files[0];
+//   fr.addEventListener('load', writeImage);
+//   fr.readAsDataURL(myFile);
+// }
+
+// function writeImage() {
+//   profileImage.src= fr.result;
+// }
+// function fakeFileClick() {
+//   fileField.click();
+// }
+// fileField.addEventListener('change', getImage);
+// uploadBtn.addEventListener('click', fakeFileClick);

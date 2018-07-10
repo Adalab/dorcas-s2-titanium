7ce89e96f87eb001e8a0d9491c2f31353cfda232
SandraCGarcia <sandra.gcanete@gmail.com>
2018-07-10 13:55:41 +0200

    añado rotacion a las flechas

--- js/main.min.js
+++ js/main.min.js
@@ -2,8 +2,8 @@
 
 //colapsables
 
-var collapsibles = document.querySelectorAll(".collapsible");
-var contenido = document.querySelectorAll(".collapsible__content");
+// var collapsibles = document.querySelectorAll(".collapsible");
+// var contenido = document.querySelectorAll(".collapsible__content");
 
 var collapsibleTriggers = document.querySelectorAll(".collapsible__label");
 console.log(collapsibleTriggers);
@@ -12,13 +12,8 @@ console.log(collapsibleTriggers);
 for (var i = 0; i < collapsibleTriggers.length; i++) {
   console.log(collapsibleTriggers[i]);
   collapsibleTriggers[i].addEventListener("click", updateCollapsibles);
-  // move.classList.add("rota");
-  // crear clase en el css con: rotate {
-//  transform: rotate(-180deg);
-//  margin-right: 0;
-//  margin-left: auto;
-// }
 }
+
 function updateCollapsibles(event) {
   // var move = document.querySelector(".fieldset__rellena--arrow");
   for (var i = 0; i < collapsibleTriggers.length; i++) {

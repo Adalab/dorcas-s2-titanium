0db1d47a487f9f197aa98df99741642df2afa565
Albametnet <alhernandezsan@gmail.com>
2018-07-13 12:25:21 +0200

    change:resetjs

--- js/reset.js
+++ js/reset.js
@@ -1,37 +1,36 @@
-// 'use strict';
-// var profilePic = document.querySelector ('.card__pic');
-// var miniPic = document.querySelector ('.mini');
-// var resetButton = document.querySelector('.reset');
-// var nameFilled = document.querySelector('.card__name');
-// var roleFilled = document.querySelector('.card__job');
+'use strict';
+var profilePic = document.querySelector ('.card__pic');
+var miniPic = document.querySelector ('.mini');
+var resetButton = document.querySelector('.reset');
+var nameFilled = document.querySelector('.card__name--name');
+var roleFilled = document.querySelector('.card__job--job');
+var iconsRemove = document.querySelector('.card__foot--social');
+var formRemove = document.querySelectorAll('.fieldset__rellena--input');
+var colorRemove = document.querySelector('#')
+function resetAll() {
+  // // paleta reset
+  // card.classList.remove('paleta-azul', 'paleta-roja', 'paleta-gris');
+  // card.classList.add('paleta-azul');
+  // // font reset
+  // cardInfo.classList.remove('font-card--comicsans', 'font-card--ubuntu', 'font-card--montserrat');
+  // cardInfo.classList.add('font-card--comicsans');
 
-// function resetForm() {
-//   document.querySelectorAll('.fieldset__rellena--input').reset();
-//   // // paleta reset
-//   // card.classList.remove('paleta-azul', 'paleta-roja', 'paleta-gris');
-//   // card.classList.add('paleta-azul');
-//   // // font reset
-//   // cardInfo.classList.remove('font-card--comicsans', 'font-card--ubuntu', 'font-card--montserrat');
-//   // cardInfo.classList.add('font-card--comicsans');
-//   // // reset links
-//   // var linkID = document.querySelectorAll('.contact-link');
-//   // for (var i = 0; i < linkID.length; i++) {
-//   //   if (linkID[i].classList.contains('contact__link--active') === true) {
-//   //     linkID[i].classList.remove('contact__link--active');
-//   //     linkID[i].classList.add('contact__link--inactive');
-//   //     linkID[i].href = "";
-//   //     linkID[i].title = "";
-//   //   }
-//   // }
+  // reset nombre y role
+  nameFilled.innerHTML = 'Nombre Apellido';
+  roleFilled.innerHTML = 'Descripcion';
 
-//   // reset nombre y role
-//   nameFilled.innerHTML = 'Nombre Apellido';
+  //reset pic
+  profilePic.style.backgroundImage = 'url(' + '../images/pollito.png' + ')';
+  miniPic.style.backgroundImage = 'url(' + '../images/pollito.png' + ' )';
 
-//   roleFilled.innerHTML = 'Front-end developer';
+  // quitar iconos
+  iconsRemove.innerHTML= '';
+  //quitar vlaores introducidos en el formulario
 
-//   //reset pic
-//   profilePic.src = "./images/pollito.png";
-//   miniPic.src = "./images/pollito.png";
-// }
+  for (var i = 0; i < formRemove.length; i++) {
+    formRemove[i].value = '';
+    console.log (formRemove);
+  }
+}
+resetButton.addEventListener('click', resetAll);
 
-// resetButton.addEventListener('click', resetForm);

8bd78927f3b3f04de930448ff23c14a79b976512
Albametnet <alhernandezsan@gmail.com>
2018-07-13 09:55:06 +0200

    cambios para reset

---
+++ js/reset.js
@@ -0,0 +1,37 @@
+// 'use strict';
+// var profilePic = document.querySelector ('.card__pic');
+// var miniPic = document.querySelector ('.mini');
+// var resetButton = document.querySelector('.reset');
+// var nameFilled = document.querySelector('.card__name');
+// var roleFilled = document.querySelector('.card__job');
+
+// function resetForm() {
+//   document.querySelectorAll('.fieldset__rellena--input').reset();
+//   // // paleta reset
+//   // card.classList.remove('paleta-azul', 'paleta-roja', 'paleta-gris');
+//   // card.classList.add('paleta-azul');
+//   // // font reset
+//   // cardInfo.classList.remove('font-card--comicsans', 'font-card--ubuntu', 'font-card--montserrat');
+//   // cardInfo.classList.add('font-card--comicsans');
+//   // // reset links
+//   // var linkID = document.querySelectorAll('.contact-link');
+//   // for (var i = 0; i < linkID.length; i++) {
+//   //   if (linkID[i].classList.contains('contact__link--active') === true) {
+//   //     linkID[i].classList.remove('contact__link--active');
+//   //     linkID[i].classList.add('contact__link--inactive');
+//   //     linkID[i].href = "";
+//   //     linkID[i].title = "";
+//   //   }
+//   // }
+
+//   // reset nombre y role
+//   nameFilled.innerHTML = 'Nombre Apellido';
+
+//   roleFilled.innerHTML = 'Front-end developer';
+
+//   //reset pic
+//   profilePic.src = "./images/pollito.png";
+//   miniPic.src = "./images/pollito.png";
+// }
+
+// resetButton.addEventListener('click', resetForm);

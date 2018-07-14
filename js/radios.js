b448eb337f7956eb90fea3572ef66e691c922aab
Claudia <cl.calerolopez@gmail.com>
2018-07-14 19:42:39 +0200

    colores

--- js/radios.js
+++ js/radios.js
@@ -1,44 +1,63 @@
 'use strict';
 
-var nombreVisor = document.querySelector('#element-name');
-var puestoVisor = document.querySelector('#element-role');
+var nombreVisor = document.querySelector('.card__name--name');
 var borderVisor = document.querySelector('.card__head--border');
-var iconBorderVisor = document.querySelector('.card__foot--icon');
-var iconVisor = document.querySelector('.iconsocial');
-var habilidadesVisor = document.querySelector('.card__foot--text');
+var iconBorderVisor = document.querySelectorAll('.card__foot--icon');
+var iconVisor = document.querySelectorAll('.iconsocial');
+var fondoHabilidades = document.querySelector('.card__foot--box');
 var colorVisor = document.querySelectorAll('.diseña__colores--paleta');
 var fuenteVisor = document.querySelectorAll('.diseña__fuentes');
-var color1 = '$dark-green-blue';
-var color2 = '$dirty-blue';
-var color3 = '$hospital-green';
-
-function definirColores() {
-  borderVisor.style.borderLeft = '5px solid' + color3;
-  iconBorderVisor.style.border = '2px solid '
-  color3;
-  iconVisor = color1;
-  habilidadesVisor = color2;
-  nombreVisor = color1;
-}
 
 
 function cambiarColor(event) {
-  var colores = event.currentTarget.id;
-  if (colores === 'red') {
-    var color1 = '$dark-green-blue';
-    var color2 = '$dirty-blue';
-    var color3 = '$hospital-green';
-
+  if ( event.currentTarget.id === 'rojo' ){
+    nombreVisor.classList.remove('gris');
+    nombreVisor.classList.add('rojo');
+    borderVisor.classList.remove('gris');
+    borderVisor.classList.add('rojo');
+    for (var e = 0; e < iconVisor.length; e++) {
+      iconVisor[e].classList.remove('gris');
+      iconVisor[e].classList.add('rojo');
+    }
+    for (var x = 0; x < fondoHabilidades.length; x++) {
+      fondoHabilidades[x].classList.remove('gris');
+      fondoHabilidades[x].classList.add('rojo');
+    }
+    for (var z = 0; z < iconBorderVisor.length; z++) {
+      iconBorderVisor[z].classList.remove('gris');
+      iconBorderVisor[z].classList.add('rojo');
+    }
+  } else if ( event.currentTarget.id === 'gris' ){
+    nombreVisor.classList.remove('rojo');
+    borderVisor.classList.remove('rojo');
+    iconBorderVisor.classList.remove('rojo');
+    iconVisor.classList.remove('rojo');
+    fondoHabilidades.classList.remove('rojo');
+    nombreVisor.classList.add('gris');
+    borderVisor.classList.add('gris');
+    iconBorderVisor.classList.add('gris');
+    iconVisor.classList.add('gris');
+    fondoHabilidades.classList.add('gris');
+  } else {
+    nombreVisor.classList.remove('rojo');
+    borderVisor.classList.remove('rojo');
+    iconBorderVisor.classList.remove('rojo');
+    iconVisor.classList.remove('rojo');
+    fondoHabilidades.classList.remove('rojo');
+    nombreVisor.classList.remove('gris');
+    borderVisor.classList.remove('gris');
+    iconBorderVisor.classList.remove('gris');
+    iconVisor.classList.remove('gris');
+    fondoHabilidades.classList.remove('gris');
   }
 }
+// function cambiarFuente() {
 
-function cambiarFuente() {
-  teste();
-}
+// }
 
 for (var i = 0; i < colorVisor.length; i++) {
   colorVisor[i].addEventListener('click', cambiarColor);
 }
-for (var i = 0; i < fuenteVisor.length; i++) {
-  fuenteVisor[i].addEventListener('click', cambiarFuente);
-}
+// for (var i = 0; i < fuenteVisor.length; i++) {
+//   fuenteVisor[i].addEventListener('click', cambiarFuente);
+// }

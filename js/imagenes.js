6fac241bf1873eaf141a5e81a285c7817e05e96f
АНЯ <annya.branco@gmail.com>
2018-07-16 23:16:07 +0200

    main.min.js

--- js/imagenes.js
+++ js/imagenes.js
@@ -5,8 +5,9 @@ var fr = new FileReader();
 var uploadBtn = document.querySelector('.añadir__upload-btn');
 var fileField = document.querySelector('#img-selector');
 var profileImage = document.querySelector('.card__pic');
-var miniBox = document.querySelector ('.mini');
-function getImage(e){
+var miniBox = document.querySelector('.mini');
+
+function getImage(e) {
   var myFile = e.currentTarget.files[0];
   fr.addEventListener('load', writeImage);
   fr.readAsDataURL(myFile);
@@ -17,10 +18,9 @@ function writeImage() {
   profileImage.style.backgroundImage = 'url(' + fr.result + ')';
   miniBox.style.backgroundImage = 'url(' + fr.result + ' )';
 }
+
 function fakeFileClick() {
   fileField.click();
 }
 fileField.addEventListener('change', getImage);
 uploadBtn.addEventListener('click', fakeFileClick);
-
-

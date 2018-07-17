f60a519778d65fe5e001d2966bfb83e6f88e26e4
АНЯ <annya.branco@gmail.com>
2018-07-17 06:45:38 +0200

    Fix: reset borra las habilidades

--- js/reset.js
+++ js/reset.js
@@ -21,8 +21,35 @@ function resetAll() {
   profileImage.style.backgroundImage = 'url(../images/pollito.png)';
   miniBox.style.backgroundImage = 'url(../images/pollito.png)';
 
+  // reset habilidades
+  removeSelect();
+  removeSelect();
+  document.querySelector('.select__habilidades1').value = '';
+  var habilidadesVisor = document.querySelectorAll('.card__foot--box');
+  for (var i = 0; i < habilidadesVisor.length; i++) {
+    habilidadesVisor[i].remove();
+  }
+  infoTarjeta = {
+    'nombre': '',
+    'puesto': '',
+    'foto': '',
+    'email': '',
+    'telefono': '',
+    'linkedin': '',
+    'github': '',
+    'habilidade1': '',
+    'habilidade2': '',
+    'habilidade3': '',
+    'color': 'azul',
+    'fuente': 'ubuntu'
+  }
+
+  localStorage.setItem('Profile-Card', JSON.stringify(infoTarjeta));
+
+  // reset localstorage
+
   // quitar iconos
-  for (var i = 0; i<iconBorderVisor.length; i++){
+  for (var i = 0; i < iconBorderVisor.length; i++) {
     iconBorderVisor[i].style.display = 'none';
   }
   //quitar vlaores introducidos en el formulario
@@ -32,4 +59,3 @@ function resetAll() {
   }
 }
 resetButton.addEventListener('click', resetAll);
-

25c7360a828ee2ab616ba160a57f3cb55d38d18b
EsterGonzalezG <estergonzalezgalindo@gmail.com>
2018-07-20 10:42:37 +0200

    vacio

--- js/name.js
+++ js/name.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var nameField = document.querySelector('.fieldset--name');
-var roleField = document.querySelector('.fieldset--role');
+const nameField = document.querySelector('.fieldset--name');
+const roleField = document.querySelector('.fieldset--role');
 
 function writeData(event){
   var guiltyElement = event.currentTarget;
   var targetID = guiltyElement.getAttribute('data-donde');
- document.querySelector('#' + targetID).innerHTML = guiltyElement.value;
+  document.querySelector('#' + targetID).innerHTML = guiltyElement.value;
 }
 nameField.addEventListener('keyup', writeData);
 roleField.addEventListener('keyup', writeData);
